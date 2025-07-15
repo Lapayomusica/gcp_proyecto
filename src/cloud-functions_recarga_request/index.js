c4daf4627bf0a7150a16fa5dc0b9dbe882e6019c
Lapayomusica <lapamusica01@gmail.com>
2025-07-14 18:09:02 -0600

    Update index.js error res

--- src/cloud-functions_recarga_request/index.js
+++ src/cloud-functions_recarga_request/index.js
@@ -8,7 +8,7 @@ app.post('/recarga', async (req, res) => {
   const {numero, monto} = req.body;
   if (!numero || !monto) return res.status(400).send('Datos incompletos');
   await pubsub.topic('recargas').publishMessage({ json: { numero, monto, fecha: new Date().toISOString() }});
-xcxc  res.status(200).send('Recarga recibida');
+  res.status(200).send('Recarga recibida');
 });
 
 const PORT = process.env.PORT || 8080;
