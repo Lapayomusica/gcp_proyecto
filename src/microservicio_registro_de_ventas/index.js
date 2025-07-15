5c1dc2437f36872da4ecf1bf7884fc885d864299
Lapayomusica <lapamusica01@gmail.com>
2025-07-15 14:35:19 -0600

    Update index.js

--- src/microservicio_registro_de_ventas/index.js
+++ src/microservicio_registro_de_ventas/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const {Firestore} = require('@google-cloud/firestore');
 const app = express();
-const firestore = new Firestore();
+const firestore = new Firestore({ projectId: 'gcp-final-465520' });
 
 app.use(express.json());
 
+app.get('/', (req, res) => res.status(200).send('OK'));
 app.post('/registrar-venta', async (req, res) => {
   const {numero, monto, fecha} = req.body;
   if (!numero || !monto) return res.status(400).send("Datos incompletos");
@@ -18,4 +19,4 @@ app.post('/registrar-venta', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Servicio de Registro de Venta escuchando en ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Servicio de Registro de Venta escuchando en ${PORT}`));
